b22ced66e157544c4647584e7cfa16f1c8cc26d5
Unknown <ulieckstein@gmail.com>
2017-10-04 15:37:07 +0200

    added settings file

---
+++ DrachenwetterLambda/Settings.cs
@@ -0,0 +1,12 @@
+﻿using KiteWeather.Models;
+
+namespace KiteWeather
+{
+    public static class Settings
+    {
+        public const string City = "Koblenz,de";
+        public const string Units = "metric";
+        public const string BaseUrl = "http://api.openweathermap.org/data/2.5/";
+        public const string AppId = "#insert app id here#";
+    }
+}
