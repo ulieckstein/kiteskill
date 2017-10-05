0e30fd56ee267f98e1b8cdfac6face1a93c0e0be
Unknown <ulieckstein@gmail.com>
2017-10-05 15:05:27 +0200

    added console test project

---
+++ DrachenwetterLambda/ConsoleTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using KiteWeather;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wcs = new WeatherConditionService();
+            Console.WriteLine("Testing output for GetCurrentKiteWeather method:");
+            Console.WriteLine(wcs.GetCurrentKiteWeather());
+
+            Console.WriteLine();
+            Console.WriteLine("press any key to exit");
+            Console.Read();
+        }
+    }
+}
