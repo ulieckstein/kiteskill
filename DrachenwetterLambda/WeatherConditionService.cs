5c972e4f24dd7772f4de69398ed43d3afc1760e0
Uli Eckstein <u.eckstein@brickmakers.de>
2017-10-28 21:48:03 +0200

    use weater prediction for output string - needs refactoring

--- DrachenwetterLambda/WeatherConditionService.cs
+++ DrachenwetterLambda/WeatherConditionService.cs
@@ -18,12 +18,12 @@ namespace KiteWeather
             var predictionTime = $"Hier das aktuelle Wetter von {current.Time.Hour} Uhr {current.Time.Minute}: ";
             if (current.Wind.SpeedKmH < 15.0)
             {
-                return $"{predictionTime} Der Wind ist mit {Math.Round(current.Wind.SpeedKmH)} Stundenkilometern heute leider zu schwach um einen Drachen steigen zu lassen.";
+                return $"{predictionTime} Der Wind ist mit {Math.Round(current.Wind.SpeedKmH)} Stundenkilometern gerade leider zu schwach um einen Drachen steigen zu lassen.";
             }
             if (current.Wind.SpeedKmH > 40)
             {
                 return
-                    $"{predictionTime} Es ist heute sehr stürmisch! Bei {Math.Round(current.Wind.SpeedKmH)} Stundenkilometern Wingeschwindigkeit solltest du mit deinem Drachen lieber zuhause bleiben.";
+                    $"{predictionTime} Es ist im Moment sehr stürmisch! Bei {Math.Round(current.Wind.SpeedKmH)} Stundenkilometern Windgeschwindigkeit solltest du mit deinem Drachen lieber zuhause bleiben.";
             }
             return
                 $"{predictionTime} Mit einer Windgeschwindigkeit von {Math.Round(current.Wind.SpeedKmH)} Stundenkilometern ist gerade ideales Wetter um einen Drachen steigen zu lassen!";
@@ -34,14 +34,18 @@ namespace KiteWeather
             var predictions = new List<Prediction> {GetCurrent().Result};
             predictions.AddRange(GetForecast().Result.List);
 
+            return CreateWindSpeedResult(predictions);
+        }
+
+        private string CreateWindSpeedResult(List<Prediction> predictions)
+        {
             var goodConditions = predictions
                 .Where(p => p.Wind.SpeedKmH < 40 && p.Wind.SpeedKmH > 15 && p.Time.Date == DateTime.Now.Date)
                 .ToList();
 
             if (goodConditions.Any())
             {
-                return
-                    $"Zwischen {goodConditions.Min(p => p.Time).Hour} und {goodConditions.Max(p => p.Time).Hour} Uhr ist bei einer durchschnittlichen Windgeschwindigkeit von {Math.Round(goodConditions.AverageWindKmH())} Stundenkilometern heute ideales Wetter um Drachen steigen zu lassen";
+                return CreateWeatherResult(goodConditions);
             }
             var average = predictions.AverageWindKmH();
             if (average > 40)
@@ -53,6 +57,22 @@ namespace KiteWeather
                 $"Der Wind ist heute mit etwa {Math.Round(average)} Stundenkilometern leider zu schwach um einen Drachen steigen zu lassen.";
         }
 
+        private string CreateWeatherResult(List<Prediction> goodWindPredictions)
+        {
+            var goodWeatherConditions = goodWindPredictions.Where(p => p.Weather.Any(w => w.Mapped.CanFly)).ToList();
+
+            if (goodWeatherConditions.Any())
+            {
+                return
+                    $"Zwischen {goodWindPredictions.Min(p => p.Time).Hour} und {goodWindPredictions.Max(p => p.Time).Hour} Uhr "+
+                    $"ist bei einer durchschnittlichen Windgeschwindigkeit von {Math.Round(goodWindPredictions.AverageWindKmH())} Stundenkilometern " +
+                    "heute ideales Wetter um Drachen steigen zu lassen" +
+                    goodWeatherConditions.ToList().GetWorstCondition().DescriptionTranslated;
+            }
+
+            return $"Wir haben zwar mit {goodWindPredictions.AverageWindKmH()} Stundenkilometern heute guten Wind, " +
+                goodWindPredictions.GetWorstCondition().DescriptionTranslated;
+        }
 
         private async Task<Forecast> GetForecast()
         {
